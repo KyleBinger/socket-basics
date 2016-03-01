4cc09fa098da1ae4b2ea7302f9d42a50ea6e26b5
Kyle Binger <kyle.a.binger@gmail.com>
2016-02-29 23:42:29 -0500

    Add front-end form for user to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -10,3 +10,19 @@ socket.on('message', function (message) {
 	console.log('New message:');
 	console.log(message.text);
 });
+
+// Handles submitting of new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+	//default requires you to constantly refresh the page, here we disbale it as socket.io is real time
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('');
+}); 
